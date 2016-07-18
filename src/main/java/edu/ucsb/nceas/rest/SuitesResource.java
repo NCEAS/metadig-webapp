1300a290063e16a3dd16ed6c047d39da06d30c2e
leinfelder <leinfelder@nceas.ucsb.edu>
2016-07-18 13:35:32 -0700

    incorporate the NVP parameter refactor from mdqengine project

--- src/main/java/edu/ucsb/nceas/rest/SuitesResource.java
+++ src/main/java/edu/ucsb/nceas/rest/SuitesResource.java
@@ -4,7 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 import java.util.Collection;
+import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -21,6 +23,8 @@ import javax.xml.bind.JAXBException;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 
 import edu.ucsb.nceas.mdqengine.Aggregator;
@@ -143,7 +147,8 @@ public class SuitesResource {
 		try {
 			Suite suite = store.getSuite(id);
 			Aggregator a = new Aggregator();
-			batchResult = a.runBatch(query, suite);
+			List<NameValuePair> params = URLEncodedUtils.parse(query, Charset.forName("UTF-8"));
+			batchResult = a.runBatch(params , suite);
 			
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
